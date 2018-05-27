7b6fd050a7760704d39f1c85963a1e58ce7556fa
lauraszyms <lauraszyms@gmail.com>
2018-05-27 01:51:56 -0400

    added post 'api/movies' route and validateMovie function

--- server/server.js
+++ server/server.js
@@ -13,6 +13,32 @@ app.get('/api/movies', (req, res) => {
   res.send(movies)
 });
 
+app.post('/api/movies', (req, res) => {
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const movie = {
+    id: movies.length + 1,
+    title: req.body.title,
+    rating: req.body.rating,
+    yearOfRelease: req.body.yearOfRelease,
+  };
+  movies.push(movie);
+  res.send(movie);
+});
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().required(),
+    rating: Joi.string().regex(/(?<!\d)(\d(?:\.[1-9])?|10)(?!\d)/).required(),
+    yearOfRelease: Joi.number().required()
+  };
+
+  return Joi.validate(movie, schema);
+
+};
