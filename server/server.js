fcca0d1f12fe2ac6c034cb2c3de8a89ffde326cc
lauraszyms <lauraszyms@gmail.com>
2018-05-27 01:40:38 -0400

    add port listener

--- server/server.js
+++ server/server.js
@@ -4,3 +4,7 @@ const app = express();
 const movies = require('../data/movies')
 
 app.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
