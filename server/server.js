aaa4f3caae7e1dc66d1156eac7659ddc61297325
lauraszyms <lauraszyms@gmail.com>
2018-05-27 01:42:50 -0400

    add / get route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const movies = require('../data/movies')
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Blockbuster!')
+  });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
