0db59d1d3431aae45cde1d6d8993ac0f0cf5482c
lauraszyms <lauraszyms@gmail.com>
2018-05-27 20:16:22 -0400

    created AddMovie component

---
+++ client/src/Components/AddMovie.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(){
+    super();
+    this.state = {
+     newMovie: {}
+    }
+  };
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+};
+
+export default AddMovie;
