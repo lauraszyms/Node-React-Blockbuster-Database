c07a68b5ba4aee492721e71a5dbb1473b3e3f07c
lauraszyms <lauraszyms@gmail.com>
2018-05-27 21:21:36 -0400

    added delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Movies from './Components/Movies'
 import AddMovie from './Components/AddMovie'
 
 class App extends Component {
-
   constructor(){
     super();
     this.state = {
@@ -32,15 +31,22 @@ class App extends Component {
     this.setState({movies: movies});
   };
 
+  handleDeleteMovie(id) {
+    let movies = this.state.movies;
+    let index = movies.findIndex(i => i.id === id);
+    movies.splice(index, 1);
+    this.setState({movies: movies});
+  };
+
   render() {
     return (
       <div className="App">
-       Welcome to Blockbuster!
-       < Movies movies={this.state.movies} />
-       < AddMovie movies={this.state.movies} addMovie={this.handleAddMovie.bind(this)}/>
+     Welcome to Blockbuster Video!
+     < Movies movies={this.state.movies} onDelete={this.handleDeleteMovie.bind(this)}/>
+     < AddMovie movies={this.state.movies} addMovie={this.handleAddMovie.bind(this)}/>
       </div>
     );
   }
-}
+};
 
 export default App;
--- client/src/Components/MovieItem.js
+++ client/src/Components/MovieItem.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class MovieItem extends Component {
 
+  deleteMovie(id) {
+    this.props.onDelete(id);
+  };
+
   render() {
     return (
       <li className="Movie">
-       <strong>{this.props.movie.title}</strong> ({this.props.movie.yearOfRelease})
+       <strong>{this.props.movie.title}</strong> ({this.props.movie.yearOfRelease}) <a href="#" onClick={this.deleteMovie.bind(this, this.props.movie.id)}><small>delete</small></a>
       </li>
     );
   }
